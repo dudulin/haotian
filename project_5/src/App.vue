7abd25ab5373675a347fb32383ae3101b260dc29
v_weidulin <v_weidulin@tencent.com>
2022-05-11 20:37:14 +0800

    * project_5 设置

--- project_5/src/App.vue
+++ project_5/src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
+  <div id="app">
     <el-row>
-      <el-col :span="24">
-        <el-button type="success" plain>导入测试环境数据</el-button>
-        <el-button type="success" plain>导入线上环境数据</el-button>
+      <el-col :span="24" style="text-align:center;margin: 50px 0;">
+        <el-button type="primary" style="margin-right:90px;">导入测试环境数据</el-button>
+        <el-button type="primary">导入线上环境数据</el-button>
       </el-col>
     </el-row>
-    <el-tabs type="border-card">
+    <el-tabs type="border-card" style="width:80vw;margin:0 8vw;">
       <el-tab-pane label="页面">
         <Page></Page>
       </el-tab-pane>
@@ -14,7 +14,11 @@
         <interface></interface>
       </el-tab-pane>
     </el-tabs>
-    <el-button type="primary" plain>校验数据</el-button>
+    <el-row>
+      <el-col :span="24" style="text-align:center;margin: 50px 0;">
+        <el-button type="primary">校验数据</el-button>
+      </el-col>
+    </el-row>
     <el-table :data="tableData" height="250" border style="width: 100%">
       <el-table-column prop="date" label="参数" width="180">
       </el-table-column>
@@ -25,7 +29,6 @@
       <el-table-column prop="address" label="校验结果">
       </el-table-column>
     </el-table>
-
   </div>
 </template>
 
@@ -74,5 +77,8 @@ export default {
 }
 </script>
 
-<style>
-</style>
+<style scoped>
+#app {
+  padding: 30px;
+}
+</style>>
